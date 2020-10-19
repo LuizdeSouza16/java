7425eb712d74935abe986cb1c6d8b3024b4b80f2
LuizdeSouza16 <luizdesouza16@gmail.com>
2020-10-19 14:23:44 -0300

    add applicaton package

---
+++ Chess/src/application/Program.java
@@ -0,0 +1,8 @@
+package application;
+
+public class Program {
+    public static void main(String[] args) {
+        String msg = "Hello World!";
+        System.out.print("Hello World!");
+    }
+}
